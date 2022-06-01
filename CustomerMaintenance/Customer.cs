9d7ca6f901c43b31ca9c5c3a5ad1fb97bc60d1d4
Fatima Pilabre <fpilabre8599@smail.pcd.edu>
2022-05-31 20:51:56 -0700

    Add project files.

---
+++ CustomerMaintenance/Customer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerMaintenance
+{
+    public class Customer
+    {
+        //private string firstName;
+        //private string lastName;
+        //private string email;
+
+        // constructor- default
+        public Customer() 
+        { 
+
+        }
+
+        // expression-bodied constructor that uses a tuple
+        public Customer(string firstname, string lastname, string email) =>
+            (this.FirstName, this.LastName, this.Email) = (firstname, lastname, email);
+
+        // auto-implemented properties
+        public string FirstName 
+        { 
+            get; 
+            set; 
+        }
+        public string LastName 
+        {
+            get;
+            set;
+        }
+        public string Email 
+        { 
+            get;
+            set; 
+        }
+        
+        // expression-bodied method
+        public string GetDisplayText() => FirstName + " " + LastName + " , " + Email;
+
+        
+
+        
+
+
+
+            
+
+
+    }
+}
